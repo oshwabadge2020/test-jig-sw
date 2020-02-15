b48cde1911fffdb7811b29431f303c77c715aea9
acamilo <alex.camilo@gmail.com>
2020-02-15 18:54:44 -0500

    test script output

--- bcolors.py
+++ bcolors.py
@@ -3,10 +3,10 @@ class bcolors:
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
+	 CYAN = '\033[96m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
-	 CYAN = '\033[96m'
     UNDERLINE = '\033[4m'
 
 def ERR(st):
