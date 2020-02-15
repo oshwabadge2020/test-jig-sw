885ce7e50a2281db0eab943f961009f5f82a2a6a
acamilo <alex.camilo@gmail.com>
2020-02-15 18:55:26 -0500

    test script output

--- bcolors.py
+++ bcolors.py
@@ -1,13 +1,13 @@
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-	 CYAN = '\033[96m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	CYAN = '\033[96m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
 
 def ERR(st):
 	print("%s%s%s%s"%(bcolors.FAIL,bcolors.BOLD,st,bcolors.ENDC))
