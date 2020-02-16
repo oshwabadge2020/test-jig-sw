c764bce9821580f7c95e35ca6319ff996289b359
acamilo <alex.camilo@gmail.com>
2020-02-16 02:58:44 -0500

    USe opencv to do the thresh

--- processimg.py
+++ processimg.py
@@ -10,4 +10,5 @@ imgPath = sys.argv[1]
 img = cv2.imread(imgPath, 0)
 kernel = np.ones((5, 5), np.uint8)
 processed=cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-cv2.imwrite('processed.png', processed)
+ret,thresh1 = cv.threshold(processed,127,255,cv.THRESH_BINARY)
+cv2.imwrite('processed.png', thresh1)
