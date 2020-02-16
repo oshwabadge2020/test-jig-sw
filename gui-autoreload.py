1b49be18b594f3f51d161071d7a97fa78dbf6f23
acamilo <alex.camilo@gmail.com>
2020-02-15 22:32:26 -0500

    clear status on fresh boot. pushing changes to device

--- gui-autoreload.py
+++ gui-autoreload.py
@@ -2,7 +2,11 @@ import os
 import subprocess
 import time
 
+firstboot=True
 while True:
+  if firstboot:
+    firstboot=False
+    os.system("echo > result.txt")
   command = "python3 gui.py"
   process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
   process.wait()
