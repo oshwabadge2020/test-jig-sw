866eef80230064d716fae8c91373e83ad800e1ec
acamilo <alex.camilo@gmail.com>
2020-02-15 23:05:06 -0500

    sometimes display is blank and needs a click

--- gui-autoreload.py
+++ gui-autoreload.py
@@ -9,5 +9,7 @@ while True:
     os.system("echo > result.txt")
   command = "python3 gui.py"
   process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+  time.sleep(2)
+  os.system("xdotool mousemove 100 100 click 1")
   process.wait()
   time.sleep(0.1)
