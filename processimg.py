fade9109ba1295d32a9a61602c234a6ae9ca1e50
Alex Camilo <alex.camilo@gmail.com>
2020-02-19 02:12:01 -0500

    Ths script now actually works

--- processimg.py
+++ processimg.py
@@ -1,14 +1,25 @@
-
 # -*- coding: utf-8 -*-
 import qrtools
 import cv2
 import numpy as np
 import sys
+import imutils
 
 imgPath = sys.argv[1]
 
-img = cv2.imread(imgPath, 0)
-kernel = np.ones((5, 5), np.uint8)
-processed=cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-ret,thresh1 = cv2.threshold(processed,60,255,cv2.THRESH_BINARY)
-cv2.imwrite('processed.png', thresh1)
+# Open up our input image
+img_ur = cv2.imread(imgPath,  cv2.IMREAD_UNCHANGED)
+
+# Camera is sligly rotated, correct.
+img = imutils.rotate(img_ur,2.5)
+
+# Crop out screen
+cropped = img[294:626, 851:1164]
+
+# Greyscale it
+grayscaled = cv2.cvtColor(cropped,cv2.COLOR_BGR2GRAY)
+
+#Threshold out the QR so zimage doesn't choke
+th = cv2.adaptiveThreshold(grayscaled, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+
+cv2.imwrite('processed.png', th)
